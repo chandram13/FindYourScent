343affef553a46bcc63df3469b3ea1c371a2d9b3
Marvish Subhas Chandra <mschandr@uci.edu>
2022-09-25 13:14:01 -0700

    Add files via upload

--- scent.kts
+++ scent.kts
@@ -21,11 +21,122 @@ fun masculine(scent,isUnisex){
         print("Use this scent when being at a feminine event or trying to attract someone who likes similar smells.")
     if isUnisex == "lavender smell":
         return true;
-    if scent == "cedar and amber":
+    if scent == "cedar & amber":
         print("These two smells deliver a clean smell that deliver an outstanding smell. This combination is low profile.")
     if isUnisex == "cedar and amber":
         return true;
+    if scent == "amber & vanilla":
+        print("These smells are for a special event such as a formal dinner or wedding. The smell is enough to be not overpowering.")
+    if isUnisex == "amber & vanilla":
+        return true;
+    if scent == "sage & rosemary":
+        print("This smell delivers an aqua odor that stands out being fresh and rich. It's casual enough for everyday events and special ones.")
+    if isUnisex == "sage & rosemary":
+        return false;
+    if scent == "citrus & woody":
+        print("This smell is ideal for a fresh odor that can be found outdoors. Use this odor when you're outside, espeically casual settings.")
+    if isUnisex == "citrus & woody":
+        return true;
+    if scent == "tobacco":
+        print("Use this odor when you're outdoors accompanied by yourself or with a friend. Best used in warmer events.")
+    if isUnisex == "tobacco":
+        return false;
+    if scent == "cardamom":
+        print("This is a youthful scent that is accompanied by a stand out seductive wood smells.")
+    if isUnisex == "cardamom":
+        return false;
+    if scent == "sweet apple & mint":
+        print("The smell delivers an odor that is fresh and sweet that presents a youthful atmoshphere.")
+    if isUnisex == "sweet apple & mint":
+        return false;
+    if scent == "alcoholic":
+        print("This smell is mainly for casual settings when hanging out with friends. If used in a more formal scenario, keep in mind about the people you're with.")
+    if isUnisex == "alcoholic":
+        return true;
+    if scent == "chestnut":
+        print("Use this scent to give yourself a fresh and warm feeling. The fragrance is meant to be worn all day.")
+    if isUnisex == "chestnut":
+        return false;
+    if scent == "melon":
+        print("This fruit melon smell is perfect for a youthul individual because of the freshness aroma.")
+    if isUnisex == "melon":
+        return false;
+    if scent == "leather":
+        print("The odor is strong, so wear it in colder environments. It's great for both casual and formal settings.")
+    if isUnisex == "leather":
+        return false;
+    if scent == "chocolate":
+        print("The smell from the chocolate gives rich and smooth tones. Known as a medium scent because the odor is present, without being overpowering.")
+    if isUnisex == "chocolate":
+        return false;
+    if scent == "gasoline":
+        print("The fragrance is bold and strong that gives it a masculine feel. The bold fragrance is meant to be outstanding. Great for colder months.")
+    if isUnisex == "gasoline":
+        return false;
+    if scent == "tobacco & leather":
+        print("This combination of scents are masculine and are reminiscient to the woods. Meant for older men, works in all settings.")
+    if isUnisex == "tobacco & leather":
+        return false;
+    else: print("Either the scent fits a feminine perfume or you have given an input that is not based on a masculine cologne.")
 }
 
 
-fun feminine(scent,isUnisex){}
+fun feminine(scent,isUnisex){
+    if scent == "freesia & rose":
+        print("The combination of scents gives a strong feminine presence. Great for garden parties, or events that are held outdoors for long periods of time.")
+    if isUnisex == "freesia & rose":
+        return false;
+    if scent == "white lily & violet":
+        print("This combination is similar to plants found in gardens. The smell is great for casual and formal events.")
+    if isUnisex == "white lily & violet":
+        return false;
+    if scent == "rich coffee & vanilla":
+        print("The combination is great for date night activities. The rich smells are meant to cause you and the ones close to you to be awake.")
+    if isUnisex == "rich coffee & vanilla":
+        return true;
+    if scent == "orange & rose":
+        print("This combination is very feminine since these aromas are meant for mature women.")
+    if isUnisex == "orange & rose":
+        return false;
+    if scent == "tuberose":
+        print("The smell is meant to be similar to spring. Since it is a light smell, it can be used for casual settings.")
+    if isUnisex == "tuberose":
+        return false;
+    if scent == "orange & grapefruit":
+        print("This combination of smells are meant to be underwhelming. These smells provide a casual, everyday perfume.")
+    if isUnisex == "orange & grapefruit":
+        return false;
+    if scent == "white tea" ? "sage":
+        print("This scent is great for daytime and everyday wear. The smell is not overpowering, yet classy.")
+    if isUnisex == "white tea" ? "sage":
+        return false;
+    if scent == "lemon":
+        print("This scent is great for the summertime or going to the beach.")
+    if isUnisex == "lemon":
+        return false;
+    if scent == "nectar & white cedarwood":
+        print("This scent is great for dates and night outs. These combination of smells are attractive.")
+    if isUnisex == "nectar & white cedarwood":
+        return false;
+    if scent == "lavender, sandalwood, & vanilla":
+        print("These combination of smells are great for travellers and those going to private events.")
+    if isUnisex == "lavender, sandalwood, & vanilla":
+        return true;
+    if scent == "strawberries & vanilla":
+        print("This combination of smells delivers spring tones that are fresh and youthful.")
+    if isUnisex == "strawberries & vanilla":
+        return false;
+    if scent == "musky & amber":
+        print("This is a great smell for individuals that are loose and want to be all around attractive.")
+    if isUnisex == "musky & amber":
+        return true;
+    if scent == "blackberry & plum":
+        print("This scent is based on fruity tones that also delivers sweetness. Great for outdoor events.")
+    if isUnisex == "blackberry & plum":
+        return true;
+    if scent == "sea salt & sage":
+        print("The scent is for earthier tones. Based on outdoor environments, as a scent it delivers.")
+    if isUnisex == "sea salt & sage":
+        return true;
+    else: print("Either the scent fits a masculine cologne or you have given an input that is not based on a feminine perfume.")
+}
